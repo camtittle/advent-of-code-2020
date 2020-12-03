de9395e92a9fe317fe03a25ce416ef0b375f5420
Cameron Tittle <cameront@apadmi.com>
2020-12-03 10:41:41 +0000

    Day 2 part 2

--- 02/02.ts
+++ 02/02.ts
@@ -3,8 +3,8 @@ import * as fs from 'fs';
 class Password {
     rule: {
         letter: string;
-        min: number;
-        max: number;
+        num1: number;
+        num2: number;
     };
 
     password: string;
@@ -16,18 +16,24 @@ class Password {
         this.password = groups[4];
         this.rule = {
             letter: groups[3],
-            min: Number.parseInt(groups[1]),
-            max: Number.parseInt(groups[2])
+            num1: Number.parseInt(groups[1]),
+            num2: Number.parseInt(groups[2])
         };
     }
 
-    isValid = (): boolean => {
+    isValidPart1 = (): boolean => {
         // https://stackoverflow.com/questions/2903542/javascript-how-many-times-a-character-occurs-in-a-string
         const regex = new RegExp(this.rule.letter, 'g');
         const matches = this.password.match(regex);
         const count = matches ? matches.length : 0;
 
-        return count >= this.rule.min && count <= this.rule.max;
+        return count >= this.rule.num1 && count <= this.rule.num2;
+    }
+
+    isValidPart2 = (): boolean => {
+        const char1 = this.password.charAt(this.rule.num1 - 1) === this.rule.letter;
+        const char2 = this.password.charAt(this.rule.num2 - 1) === this.rule.letter;
+        return (char1 && !char2) || (!char1 && char2);
     }
 }
 
@@ -36,14 +42,15 @@ class Password {
     const file: string = await fs.promises.readFile(__dirname + '/' + fileName, 'utf8');
     const lines = file.split('\r\n');
 
-    const passwords: Password[] = lines.map(line => {
-        return new Password(line);
-    });
+    const passwords = lines.map((line) => new Password(line));
 
-    const correctCount = passwords.reduce((count, password) => {
-        const valid = password.isValid();
-        return valid ? ++count : count;
+    const correctCountPart1 = passwords.reduce((count, password) => {
+        return password.isValidPart1() ? ++count : count;
     }, 0);
+    console.log('Part 1 Correct passwords count: ' + correctCountPart1);
 
-    console.log('Correct passwords count: ' + correctCount);
+    const correctCountPart2 = passwords.reduce((count, password) => {
+        return password.isValidPart2() ? ++count : count;
+    }, 0);
+    console.log('Part 2 Correct passwords count: ' + correctCountPart2);
 })();
